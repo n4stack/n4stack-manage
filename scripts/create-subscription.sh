11c4bda221b3797170f535a94a1a0b4fa64b6f49
n.wardle@node4.co.uke <n.wardle@node4.co.uk>
2022-05-16 16:47:50 +0100

    .

--- scripts/create-subscription.sh
+++ scripts/create-subscription.sh
@@ -1,6 +1,6 @@
 
 # Create Azure Subscription Example
-az account create --enrollment-account-name=n.wardle@node4.co.uk --display-name=n.wardle@node4.co.uk --offer-type MS-AZR-0148P
+az account create --enrollment-account-name='nigel Wardle' --display-name=test-sub --offer-type MS-AZR-0148P
 ------------------------------------------------------------
 
 
--- scripts/set-azure-policy.sh
+++ scripts/set-azure-policy.sh
@@ -1,20 +1,18 @@
+
 # create a resource group
-l=northeurope
-rg=ne-rg-gp011991
-az group create -l $l -n $rg
+az group create -l northeurope -n ne-rg-gp011991
 
 # Retreive built in policy definition
 definition=0473574d-2d43-4217-aefe-941fcdf7e684
 
 # set scope of group policy
-scope=$(az group show --name $rg --output tsv --query id)
+scope=$(az group show --name ne-rg-gp011991 --output tsv --query id)
 
 # set policy params
 policyparam='{ "listOfAllowedLocations": { "value": ["uksouth", "ukwest", "northeurope"]}}'
 
 # create policy on resource group
-az policy assignment create --name 'allowedLocations' --display-name 'allowedLocations' \
-  --scope $scope --policy $definition --params "$policyparam" --output jsonc
+az policy assignment create --name 'allowedLocations' --display-name 'allowedLocations' --scope $scope --policy $definition --params "$policyparam" --output jsonc
 
 
 
