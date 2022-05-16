63d2a581d42b4b7e79daa3394b5da8c8ecff65b6
n.wardle@node4.co.uke <n.wardle@node4.co.uk>
2022-05-16 16:17:41 +0100

    .

--- scripts/storage-account.sh
+++ scripts/storage-account.sh
@@ -1,15 +1,17 @@
 # Request a storage Account Example
 # az storage account create --resource-group rg-011991 --name sa01991  -l northeurope --sku Standard_LRS
 
-
-rg=rg-4-sa01881
-sa-name=sa01881
+rg=rg-sa01881
 location=northeurope
-sku=Standard_LRS
 
-echo "Attempting to creating storage account..."
+echo "Creating Resource Group..."
 
 az group create -l $location -n $rg
 
-az storage account create --resource-group $rg --name $sa-name  -l $location --sku $sku
+echo "Creating storage account..."
+
+saname=sa01881
+sku=Standard_LRS
+
+az storage account create --resource-group $rg --name $saname  -l $location --sku $sku
 
--- scripts/vm-in-vnet.sh
+++ scripts/vm-in-vnet.sh
@@ -1,27 +1,24 @@
-# Request a storage Account Example
-# az storage account create --resource-group rg-011991 --name sa01991  -l northeurope --sku Standard_LRS
-
-
-rg=rg-vnet-011991
-vnet-name=ne-vnet011991
-location=northeurope
-vnet-addr=10.220.0.0/16
-snet-addr=10.220.0.0/24
-snet-name=ne-sn011991
-sku=Standard_LRS
+# Create a VM in a new VNET example automation
 
 echo "Running steps to provision an Ubuntu VM into a new VNET..."
 
 # Request a resource group
-az group create -l $location -n $rg
+az group create -l northeurope -n rg-vm-app-011991
+
+echo
+echo "Creating a VNET..."
 
 # Request a VNET
-az network vnet create -g $rg -n $vnet-name --address-prefix $vnet-addr --location $location \
-  --subnet-name $snet-name  -sn --subnet-prefix $snet-addr
+az network vnet create -g rg-vm-app-011991 -n MyVnet --address-prefix 10.0.0.0/16 --subnet-name MySubnet --subnet-prefix 10.0.0.0/24 -l northeurope
+
+echo
+echo "Creating an Ubuntu VM..."
 
 # Request a VM 
-az vm create -n uks-vm-01191 --location $location -g $rg \
-   --image UbuntuLTS --subnet northeurope-vnet --size Standard_D2s_v3 --generate-ssh-keys
+az vm create -n my-vm  --location northeurope -g rg-vm-app-011991 --image UbuntuLTS --subnet MySubnet --size Standard_D2s_v3 --vnet-name MyVnet --generate-ssh-keys 
+
+echo
+echo "finished..."
 
 
 
