560f53dfb42bb66ceae5023b2323a4515f6036c0
n.wardle@node4.co.uke <n.wardle@node4.co.uk>
2022-05-16 15:40:44 +0100

    .

---
+++ scripts/subscription.sh
@@ -0,0 +1,6 @@
+
+# Create Azure Subscription Example
+az account create --enrollment-account-name=n.wardle@node4.co.uk --display-name=n.wardle@node4.co.uk --offer-type MS-AZR-0148P
+------------------------------------------------------------
+
+
