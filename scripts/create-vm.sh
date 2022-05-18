cc810ceb2a18b5389ceb7b6fd1993e14cfc41ce7
n.wardle@node4.co.uke <n.wardle@node4.co.uk>
2022-05-18 09:42:07 +0100

    .

--- scripts/create-vm.sh
+++ scripts/create-vm.sh
@@ -1,16 +1,14 @@
 # Create a VM in a new VNET example automation
-
 echo
 echo "Please enter details to create a new VM. Use Control-C to exit."
+echo
 read -p 'Resource Group - e.g: rg-myvm ' rg
 read -p 'VM Name - e.g: myvm01 ' vmname
-read -p 'VNET Name - e.g: vnet01 ' vname
-read -p 'Subnet Name - e.g: snet01 ' sname
-
+read -p 'VNET Name (that currently exists) - e.g: vnet01 ' vname
+read -p 'Subnet Name (that currently exists) - e.g: snet01 ' sname
 echo
 echo "You have entered $rg $vmname $vname $sname "
 echo 
-
 read -p "Do you want to proceed? (yes/no) " yn
 
 case $yn in 
@@ -20,6 +18,14 @@ case $yn in
 	* ) echo invalid response;
 		exit 1;;
 esac
+
+echo
+echo ".. Creating Resource Group"
+echo
+
+# Request a resource group
+az group create -l northeurope -n $rg
+echo
 
 echo
 echo "Creating an Ubuntu VM..."
