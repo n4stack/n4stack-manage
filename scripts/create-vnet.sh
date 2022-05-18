a8f7e3646e52189b9b94a8dab3f292d83b3d821a
Nigel Wardle <nigel_wardle@hotmail.com>
2022-05-18 12:46:31 +0100

    Update create-vnet.sh

--- scripts/create-vnet.sh
+++ scripts/create-vnet.sh
@@ -2,12 +2,15 @@
 # Create a VM in a new VNET example automation
 
 echo "Please enter details to create a new VNET."
+read -p 'Resource Group - e.g: rgmyvm ' rg
+read -p 'location - e.g: northeurope uksouth ukwest ' l
 read -p 'VNET Name - e.g: vnet01 ' vname
 read -p 'Subnet Name - e.g: snet01 ' sname
 read -p 'VNET Address Prefix - e.g: 10.100.0.0/16   ' vprefix
 read -p 'Subnet Address Prefix - e.g: 10.100.0.0/24   ' sprefix
+read -p 'Please enter tags for this resource. E.g: tier=dev   ' tags
 
-echo "These are your entered values: " $vname $sname $vprefix $sprefix
+echo "These are your entered values: " $vname $sname $vprefix $sprefix $tags
 
 read -p "Do you want to proceed? (yes/no) " yn
 
@@ -19,21 +22,18 @@ case $yn in
 		exit 1;;
 esac
 
-rg="rg-"$vname
-l=northeurope
-
 echo
 echo ".. Creating Resource Group"
 echo
 
 # Request a resource group
-az group create -l northeurope -n $rg
+az group create -l $l -n $rg
 echo
 echo "Creating a VNET..."
 echo
 
 # Request a VNET
-az network vnet create -g $rg -n $vname --address-prefix vprefix --subnet-name $sname --subnet-prefix $sprefix -l $l
+az network vnet create -g $rg -n $vname --subnet-name $sname --address-prefix $vprefix --subnet-prefix $sprefix -l $l  --tags $tags
 echo
 echo "finished..."
 echo
