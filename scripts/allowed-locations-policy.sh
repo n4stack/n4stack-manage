e11f9445778b34bcc187070d8a24993f52d70206
Nigel Wardle <nigel_wardle@hotmail.com>
2022-05-18 13:25:26 +0100

    Update allowed-locations-policy.sh

--- scripts/allowed-locations-policy.sh
+++ scripts/allowed-locations-policy.sh
@@ -1,10 +1,7 @@
 
-echo "Please enter your a Resource Group that you limit locations to UKSouth, UKWest. E.g rg-uk-only"
 
-read rg
-
+read -p 'Please enter your a Resource Group that you limit locations to UKSouth, UKWest. E.g rg-uk-only ' rg
 echo "you have entered $rg "
-
 read -p "Do you want to proceed? (yes/no) " yn
 
 case $yn in 
@@ -16,7 +13,7 @@ case $yn in
 esac
 
 echo
-echo ".. now creating Resource Group $rg if not exist"
+echo ".. now creating Resource Group (if not exist)"
 echo
 az group create -l northeurope -n $rg
 
@@ -24,14 +21,14 @@ az group create -l northeurope -n $rg
 definition=0473574d-2d43-4217-aefe-941fcdf7e684
 
 # set scope of group policy
-scope=$(az group show --name ne-rg-gp011991 --output tsv --query id)
+scope=$(az group show --name $rg --output tsv --query id)
 
 # set policy params
-policyparam='{ "listOfAllowedLocations": { "value": ["uksouth", "ukwest", "northeurope"]}}'
+policyparam='{ "listOfAllowedLocations": { "value": ["uksouth", "ukwest"]}}'
 
 # create policy on resource group
-az policy assignment create --name 'allowedLocations' --display-name 'allowedLocations' --scope $scope --policy $definition --params "$policyparam" --output jsonc
+az policy assignment create --name 'allowedLocations' --display-name 'allowedLocations' \
+--scope $scope --policy $definition --params "$policyparam" --output jsonc
 
 echo
 echo "... finished"
-
